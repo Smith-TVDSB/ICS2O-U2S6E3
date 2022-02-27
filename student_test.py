8b036aa5783cb2460587c5473d4698761a3e4ea9
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2022-02-27 15:11:04 -0500

    Update student_test.py

--- student_test.py
+++ student_test.py
@@ -3,16 +3,16 @@ import student
 
 
 def test_3(capsys):
-    student.pyramid(3)
+    student.pyramid(3,'oaks')
     out,err =  capsys.readouterr()
-    assert out == "*\n**\n***" or out == "* \n* * \n* * *\n"
+    assert out == "*oaks\noaksoaks\noaksoaksoaks" or out == "oaks \noaks oaks \noaks oaks oaks\n"
 
 def test_1(capsys):
-    student.pyramid(1)
+    student.pyramid(1, 'magiscarps')
     out,err =  capsys.readouterr()
-    assert out == "*\n" or out == "*"
+    assert out == "magiscarps\n" or out == "magiscarps \n" or out == 'magiscarps' or out == 'magiscarps '
 
 def test_5(capsys):
-    student.pyramid(5)
+    student.pyramid(5,'*')
     out,err =  capsys.readouterr()
     assert out == "*\n**\n***\n****\n******\n" or out == "* \n* * \n* * * \n* * * * \n* * * * * * \n"
